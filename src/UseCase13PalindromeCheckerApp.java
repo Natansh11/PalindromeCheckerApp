05faae4a9c5aa3a254c994b77a5496aa3c574a96
Natansh11 <natashkumar016@gmail.com>
2026-03-13 10:26:35 +0530

    Added UC8 to UC13 palindrome implementations

---
+++ src/UseCase13PalindromeCheckerApp.java
@@ -0,0 +1,29 @@
+public class UseCase13PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "level";
+
+        long startTime = System.nanoTime();
+
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+
+        }
+
+        long endTime = System.nanoTime();
+
+        long executionTime = endTime - startTime;
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Execution Time : " + executionTime + " ns");
+
+    }
+}
